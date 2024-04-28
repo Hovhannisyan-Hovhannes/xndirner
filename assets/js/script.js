e2b1b98c8aad744388d2cf8a1d03e66a86b22621
Hovhannes190887 <hovhannisyan19081987@gmail.com>
2024-04-28 10:54:14 +0400

    xndir 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,24 +34,47 @@
 // տրված զանգվածների դրական տարրերի ընդհանւոիր քանակը
 
 
-let x =[2,1,-8,5,-9,4];
+// let x =[2,1,-8,5,-9,4];
 
-let y =[4,5,-7,4,-8,3];
+// let y =[4,5,-7,4,-8,3];
+
+// let sum1 = 0;
+// let sum2 = 0;
+
+// for (let i = 0; i< x.length; i++){
+//     if(x[i]>0){
+//         sum1++
+//     }  
+// }
+// for (let i = 0; i< y.length; i++){
+//     if(y[i]>0){
+//         sum2++
+//     }
+// }
+// console.log(sum1+sum2)
+
+// 3
+//  տրված զանգվածների բացասական տարրերի քանակը
+
+let x = [8,-8,7,5,66,-88,-5];
+
+let y = [-7,55,99,-99,7,3,-6];
 
 let sum1 = 0;
 let sum2 = 0;
 
-for (let i = 0; i< x.length; i++){
-    if(x[i]>0){
+for(let i = 0; i< x.length; i++){
+    if(x[i]<0){
         sum1++
-    }  
+    }
 }
-for (let i = 0; i< y.length; i++){
-    if(y[i]>0){
+for(let i = 0; i<y.length; i++){
+    if(x[i]<0){
         sum2++
     }
 }
-console.log(sum1+sum2)
+console.log(sum1+sum2);
+
 
 
 
